f093bfda36cb1a22eb72e6aae82d3fdb4c835e7d
Jenushanth <sivakumarjenushanth@gmail.com>
2022-06-23 18:47:20 +0530

    Revert "server folder is added"
    This reverts commit b91240d8916cd909efa9b62de8961eba93bcd05e.

--- carousel-server/routes/carouselRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { fetchData } from "../controller/carouselController.js";
-const router = express.Router();
-
-router.get("/carousel", fetchData);
-// module.exports = router
-
-export default router;
